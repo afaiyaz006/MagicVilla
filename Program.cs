ae0055b12a37943969904945fdbdbeb304d06a7e
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2025-07-18 20:34:38 +0600

    added basic database migration

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 
+using MagicVilla_API.Data;
 using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
-
 var builder = WebApplication.CreateBuilder(args);
-Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo
-    .File("log/logs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+// Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo
+//     .File("log/logs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
 // Add services to the container.
-builder.Host.UseSerilog();
+// builder.Host.UseSerilog();
+builder.Services.AddDbContext<ApplicationDBContext>(option => {
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpLogging(logging =>
