affa464908dac1d1bdd0cc6cd61cf2734da56174
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2025-07-22 15:40:36 +0600

    added basic mvc controller

--- MagicVilla-Web/Controllers/VillaController.cs
+++ MagicVilla-Web/Controllers/VillaController.cs
@@ -1,9 +1,11 @@
+using System.Text.Json;
 using AutoMapper;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace MagicVilla_Web.Controllers;
 
@@ -11,23 +13,33 @@ public class VillaController:Controller
 {
     private readonly IVillaService _villaService;
     private readonly IMapper _mapper;
-
-    private VillaController(IVillaService villaService, IMapper mapper)
+    
+    public VillaController(IVillaService villaService, IMapper mapper)
     {
         _villaService = villaService;
         _mapper = mapper;
     }
-    
+ 
     
     public async Task<IActionResult> IndexVilla()
     {
+        
         List<VillaDTO> list = new();
-        var response = await _villaService.GetAllAsync<APIResponse>();
-        if (response != null && response.IsSuccess)
-        {
-            list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
-            
-        }
-        return View(list);
+        // var response = await _villaService.GetAllAsync<APIResponse>();
+        // Console.WriteLine(response.StatusCode);
+        // Console.WriteLine(response.IsSuccess);
+        //
+        // if (response != null && response.IsSuccess)
+        // {
+        //     list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+        //     
+        // }
+        var response = await _villaService.GetAllAsync<List<VillaDTO>>();
+        return View(response);
     }
+
+    // public IActionResult IndexVilla()
+    // {
+    //     return View();
+    // }
 }
